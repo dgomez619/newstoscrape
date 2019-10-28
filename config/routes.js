e9a7dc876fa4f5142d9484250bdfc0b1cea266c2
Daniel Gomez <ss.im.back@gmail.com>
2019-10-27 21:22:11 -0700

    not working

---
+++ config/routes.js
@@ -0,0 +1,68 @@
+module.exports = function(router){
+    //for home page
+    router.get("/", function(req, res){
+        res.render("home");
+    });
+    //for saved page
+    router.get("/saved", function (req, res) {
+        res.render("saved");
+    });
+
+    router.get("/api/fetch", function(req, res){
+        headlinesController.fetch(function(err,docs){
+            if (!docs || docs.insertedCount === 0) {
+                res.json({
+                    message: "No new articles today, check back tomorrow!"
+                });
+            }
+        });
+    });
+    router.get("/api/headlines", function(req, res){
+        var query = {};
+        if (req.query.saved){
+            query = req.query;
+        }
+
+        headlinesController.get(query, function(data){
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/headlines/:id", function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function(err, data){
+            res.json(data);
+        });
+    });
+
+    router.patch("/api/notes/headlines", function(req, res){
+        headlinesController.update(req.body, function(err,data){
+            res.json(data);
+        });
+    });
+
+    router.get("api/notes/:headline_id?", function(req, res){
+        var query = {};
+        if (req.params.headline_id){
+            query._id = req.params.headline_id;
+        }
+
+        notesController.get(query, function(err, data){
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/notes/:id", function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        notesController.delete(query, function(err, data){
+            res.json(data);
+        });
+    });
+    router.post("/api.notes", function(req, res){
+        notesController.save(req.body, function(data){
+            res.json(data);
+        })
+    })
+}
